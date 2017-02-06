dfbf8b9e40cc55f6b51113128c05f519ca0f8e3a
maaatt62 <cauadc@CAUADC-12.local>
2017-02-06 19:46:08 +0900

    Kkk

---
+++ TravelRecorder/TravelRecorder/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  TravelRecorder
+//
+//  Created by CAUADC on 2017. 2. 6..
+//  Copyright © 2017년 owlsogul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell
+        cell = tableView.dequeueReusableCell(withIdentifier: "mainCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
