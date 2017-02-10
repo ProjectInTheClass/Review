12cd839a6eaea091f4b199097462424317923f94
hi <cauadc@CAUADC-12.local>
2017-02-10 14:50:09 +0900

    Photo2

--- TravelRecorder/TravelRecorder/PhotoViewController.swift
+++ TravelRecorder/TravelRecorder/PhotoViewController.swift
@@ -7,16 +7,50 @@
 //
 
 import UIKit
+import RealmSwift
 
-class PhotoViewController: UIViewController, UIImagePickerControllerDelegate {
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    
-    
+    @IBOutlet weak var StackView: UIStackView!
     @IBOutlet weak var PhotoImageView: UIImageView!
     @IBOutlet weak var PhotoTextView: UITextView!
-    @IBAction func clickAddButton(_ sender: UIButton) {
+    
+    var photoInfos: Results<PhotoInfo>?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let realm = try? Realm()
+        self.photoInfos = realm?.objects(PhotoInfo.self)
+        
     }
     
+    @IBAction func clickAddButton(_ sender: UIButton) {
+        print("Add Button이 Click 되었습니다.")
+        
+        let actionSheet: UIAlertController
+        actionSheet = UIAlertController(title: "사진 선택", message: "사진을 가져오시겠습니까?", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        let cancelAction: UIAlertAction
+        cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { (UIAlertAction) in
+            print("사용자가 Cancel을 누름")
+            
+        
+        })
+    }
+    func showImagePicker(type: UIImagePickerControllerSourceType) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = type
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("사용자가 사진을 선택했습니다")
+        
+    }
     
     
     
