bce3a69222092419dd772bbbded8823609f12de9
eurimedas <imedas@naver.com>
2017-02-13 16:44:47 +0900

    이벤트 추가 페이지 추가
    기존 메인에서 분

---
+++ TravelRecorder/TravelRecorder/AddEventViewController.swift
@@ -0,0 +1,72 @@
+//
+//  AddEventViewController.swift
+//  TravelRecorder
+//
+//  Created by CAUADC on 2017. 2. 13..
+//  Copyright © 2017년 owlsogul. All rights reserved.
+//
+
+import UIKit
+
+class AddEventViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var saveExitButton: UIButton!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: MyCustomerTableViewCell
+        
+        if indexPath.row == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "willSaveAndExitCell", for: indexPath) as! MyCustomerTableViewCell
+            return cell
+        } else if indexPath.row == 1 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "eventTitleInputCell", for: indexPath) as! MyCustomerTableViewCell
+            return cell
+        } else if indexPath.row == 2 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "eventWithInputCell", for: indexPath) as! MyCustomerTableViewCell
+            return cell
+        } else {
+            cell = tableView.dequeueReusableCell(withIdentifier: "eventShowingPicInput", for: indexPath) as! MyCustomerTableViewCell
+            return cell
+        }
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        saveExitButton.layer.cornerRadius = saveExitButton.frame.size.width / 2
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
