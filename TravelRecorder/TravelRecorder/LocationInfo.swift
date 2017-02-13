3233ce5f07db1a9b14077e34379e17956d3cbcf7
simba328 <simba328@naver.com>
2017-02-13 16:14:08 +0900

    Alert Textfield 데이터 받아오기

---
+++ TravelRecorder/TravelRecorder/LocationInfo.swift
@@ -0,0 +1,15 @@
+//
+//  LocationInfo.swift
+//  TravelRecorder
+//
+//  Created by CAUADC on 2017. 2. 13..
+//  Copyright © 2017년 owlsogul. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class LocationInfo: Object {
+    dynamic var labelLocation = ""
+    dynamic var detailLocation = ""
+}
