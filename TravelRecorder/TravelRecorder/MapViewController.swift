14f07f2bfda8f9d1befc27396495a9062551494e
simba328 <simba328@naver.com>
2017-02-13 15:17:58 +0900

    경로추가 팝업창
    텍스트필드 띄우기

--- TravelRecorder/TravelRecorder/MapViewController.swift
+++ TravelRecorder/TravelRecorder/MapViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    
+    @IBAction func addLocation(_ sender: Any) {
+        
+    
+        
+        print("경로 추가 버튼을 눌렀습니다")
+        // 경로 추가 팝업창 띄우기
+        let alertController = UIAlertController(title: "경로를 추가해주세요", message: "상세경로는 지도에 표시됩니다", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (result : UIAlertAction) -> Void in
+            print("경로 추가 취소버튼을 눌렀습니다")})
+        let addAction = UIAlertAction(title: "Add", style: .default, handler: { (result : UIAlertAction) -> Void in
+            print("경로 추가 확인버튼을 눌렀습니다")})
+        alertController.addAction(cancelAction)
+        alertController.addAction(addAction)
+        
+        // 팝업창에 텍스트필드 띄우기
+        // placeholder은 필드 설명을 위한 내용
+        alertController.addTextField(configurationHandler: { (textField : UITextField) -> Void in
+            textField.placeholder = "경로" })
+        alertController.addTextField(configurationHandler: { (textField : UITextField) -> Void in
+            textField.placeholder = "상세경로" })
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
